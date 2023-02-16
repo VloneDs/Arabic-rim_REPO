92c8698667112e4b8b798ca6120d4007051148d4
VloneDSb <kozachekivan60@gmail.com>
2023-02-16 18:13:24 +0300

    update 2.1.1
    mini patch

--- roman.py
+++ roman.py
@@ -1,10 +1,10 @@
-def roman_to_int(s:str)->int:
+def roman_to_int(number_1:str)->int:
     res=0
     roman_number = {'CM':900, 'CD':400, 'XC': 90, 'XL': 40, 'IX': 9, 'IV':4,
         'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
     for number in roman_number:
-        res += roman_number[number] * s.count(number)
-        s = s.replace(number, '')
+        res += roman_number[number] * number_1.count(number)
+        number_1 = number_1.replace(number, '')
     return res
 
 def int_to_roman(num:int)->str:
